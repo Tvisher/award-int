7b8260f1d889869362c7892b824031ac6d3baf3b
Tvisher <tvisherfine@gmail.com>
2026-03-13 22:24:57 +0500

    +

--- js/main.js
+++ js/main.js
@@ -23,3 +23,21 @@ $(".faq-item__head").on("click", function () {
 
     });
 });
+
+
+
+document.addEventListener('click', (e) => {
+    const target = e.target;
+    if (target.closest('[data-show-modal]')) {
+        e.preventDefault()
+        const modalType = target.closest('[data-show-modal]').getAttribute('data-show-modal');
+        console.log(modalType);
+        const currentModal = document.querySelector(`[data-modal="${modalType}"]`);
+        currentModal.classList.add('show');
+    }
+
+    if (target.closest('.modal-template') && !target.closest('.modal-content') || target.closest('.form-tmp__close')) {
+        const activeModal = target.closest('.modal-template.show');
+        activeModal && activeModal.classList.remove('show');
+    }
+})
